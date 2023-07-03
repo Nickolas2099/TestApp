ee86696bff46dd10884f917436dc724852fd4215
Nickolas2099 <iantoshka2003@mail.ru>
2023-07-03 15:12:49 +0300

    implement createPost.html, a little fix ribbon.html and db entities

--- src/main/java/com/nickolas/TextBlog/Posting/User.java
+++ src/main/java/com/nickolas/TextBlog/Posting/User.java
@@ -4,10 +4,12 @@ package com.nickolas.TextBlog.Posting;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "user")
 public class User {
 
     @Id
