e974c2bdefa54e7a1f0e0319332ca0001cd6ab90
Nickolas2099 <iantoshka2003@mail.ru>
2023-07-04 14:31:44 +0300

    create services, update html documents and modify controller

--- src/main/java/com/nickolas/TextBlog/Repositories/UserRepository.java
+++ src/main/java/com/nickolas/TextBlog/Repositories/UserRepository.java
@@ -2,7 +2,9 @@ package com.nickolas.TextBlog.Repositories;
 
 import com.nickolas.TextBlog.Posting.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
